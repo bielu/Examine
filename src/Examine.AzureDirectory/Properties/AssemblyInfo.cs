2c40c1273c6730040a19f81c42f3a5d6d7c56eb4
Arkadiusz Biel <abi@novicell.co.uk>
2021-06-11 11:08:41 +0200

    bump ver

--- src/Examine.AzureDirectory/Properties/AssemblyInfo.cs
+++ src/Examine.AzureDirectory/Properties/AssemblyInfo.cs
@@ -30,6 +30,6 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: AssemblyInformationalVersion("1.0.0-novicellbeta.47")]
+[assembly: AssemblyInformationalVersion("1.0.0-novicellbeta.48")]
 
 [assembly: InternalsVisibleTo("Examine.Test")]
